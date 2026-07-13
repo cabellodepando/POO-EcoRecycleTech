f0a7e7a33a23f912b13b3ea5ad52e8f7bbba5d6b
cabellodepando <acabello.ac@gmail.com>
2026-07-13 21:41:59 +0200

    Create ResiduoPlastico.java

---
+++ java/modelo/ResiduoPlastico.java
@@ -0,0 +1,26 @@
+package modelo;
+
+public class ResiduoPlastico extends Residuo {
+    public ResiduoPlastico(String id, double peso) { super(id, peso); }
+    @Override public String getTipo() { return "Plástico"; }
+    @Override public boolean esReciclable() { return true; }
+}
+
+public class ResiduoVidrio extends Residuo {
+    public ResiduoVidrio(String id, double peso) { super(id, peso); }
+    @Override public String getTipo() { return "Vidrio"; }
+    @Override public boolean esReciclable() { return peso < 10.0; }
+}
+
+public class ResiduoPapel extends Residuo {
+    public ResiduoPapel(String id, double peso) { super(id, peso); }
+    @Override public String getTipo() { return "Papel"; }
+    @Override public boolean esReciclable() { return true; }
+}
+
+// Residuos Metal: Requisito de extensibilidad (Excelente)
+public class ResiduoMetal extends Residuo {
+    public ResiduoMetal(String id, double peso) { super(id, peso); }
+    @Override public String getTipo() { return "Metal"; }
+    @Override public boolean esReciclable() { return true; }
+}
